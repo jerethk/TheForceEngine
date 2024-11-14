a41e2427435e79e8feb1ebcb705b603832cbab24
Jereth <jerethk@iinet.net.au>
2024-11-15 04:53:38 +1100

    Handle gasmask and nightvision

--- TheForceEngine/TFE_DarkForces/mission.cpp
+++ TheForceEngine/TFE_DarkForces/mission.cpp
@@ -952,7 +952,7 @@ namespace TFE_DarkForces
 
 	void enableNightVision()
 	{
-		if (!s_playerInfo.itemGoggles) { return; }
+		if (!s_playerInfo.itemGoggles || s_externalCameraMode) { return; }
 
 		if (!s_batteryPower)
 		{
@@ -1001,7 +1001,7 @@ namespace TFE_DarkForces
 
 	void enableMask()
 	{
-		if (!s_playerInfo.itemMask) { return; }
+		if (!s_playerInfo.itemMask || s_externalCameraMode) { return; }
 
 		if (!s_batteryPower)
 		{
