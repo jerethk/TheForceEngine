8e7d2e5623e054b40efbeffe1f57989c1892dada
Jereth <jerethk@iinet.net.au>
2025-04-14 01:51:05 +1000

    Add pickup scriptcall

--- TheForceEngine/TFE_DarkForces/pickup.cpp
+++ TheForceEngine/TFE_DarkForces/pickup.cpp
@@ -331,6 +331,12 @@ namespace TFE_DarkForces
 			sound_play(s_itemPickupSnd);
 		}
 
+		// TFE - Call Pickup script
+		if (pickup->pickupScriptCall.funcPtr)
+		{
+			TFE_ForceScript::execFunc(pickup->pickupScriptCall.funcPtr, 0);
+		}
+
 		// Initialize effect
 		s_flashEffect = FIXED(15);
 		task_makeActive(s_pickupTask);
@@ -469,6 +475,9 @@ namespace TFE_DarkForces
 		pickup->msgId[0] = -1;
 		pickup->msgId[1] = -1;
 		pickup->maxAmount = 999;
+		
+		pickup->pickupScriptCall.funcPtr = nullptr;
+		pickup->pickupScriptCall.objectId = -1;
 
 		setPickup(pickup, obj, id, TFE_ExternalData::getExternalPickups());
 		return (Logic*)pickup;
