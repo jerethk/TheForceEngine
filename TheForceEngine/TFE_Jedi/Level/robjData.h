a58a1d76818daeadacb8838c84223f0fae139797
Jereth <jerethk@iinet.net.au>
2025-04-18 21:03:33 +1000

    Serialization

--- TheForceEngine/TFE_Jedi/Level/robjData.h
+++ TheForceEngine/TFE_Jedi/Level/robjData.h
@@ -66,7 +66,8 @@ enum ObjStateVersion : u32
 	ObjState_CustomLogics = 6,
 	ObjState_ConstOverrides = 7,
 	ObjState_DisablePlayerMovement = 8,
-	ObjState_CurVersion = ObjState_DisablePlayerMovement,
+	ObjState_ExternalCamera = 9,
+	ObjState_CurVersion = ObjState_ExternalCamera,
 };
 
 #define SPRITE_SCALE_FIXED FIXED(10)
